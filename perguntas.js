2a0ee6dc5230179db5a62d2534789ace126a1fd1
Millenacris <159458321+Millenacris@users.noreply.github.com>
2024-09-12 08:53:06 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,68 @@
-criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
-)
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Qual o unico pais onde os coalas vivem?',
+    'Australia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Matemática',
+    ' Quanto é 7+5?',
+    '12'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Lingua Portuguêsa',
+    'Qual o plural de pão?',
+    'Pães'
 )
+
+criaCartao(
+'História',
+'Em que ano declarou a independencia do Brasil?',
+'1822'
+)
+
+criaCartao(
+'Ciênsia',
+'Qual o planeta mais procimo do sol?',
+'Mercúrio'
+)
+
+criaCartao(
+'Inglês',
+'Como se diz gato em inglê?',
+'Cat'
+)
+
+criaCartao(
+'Educação Física',
+'Quantos jogadores formam uma equipe de futebol?',
+'11 jogadores' 
+)
+
+criaCartao(
+'Arte',
+'Qual artista pintou a Mona Lisa?',
+ 'Leonardo da Vinci'   
+)
+
+criaCartao(
+'Física',
+'qual é a unidade de medida de força no Sistema Internacional?',
+ 'Newton(N)'   
+)
+
+criaCartao(
+
+
+    
+)
+
+criaCartao(
+
+
+    
+)
+
+
